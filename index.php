bd839c861f13a32475de4d32a1776874e8bcd0ad
Xenophmix <xenophmix@gmail.com>
2022-12-16 10:39:07 +0800

    物件中方法及權限的關係

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
 
 
+class Animal
+{
+  protected $type;
+  protected $name;
+  protected $hair_color;
 
+  public function __construct($type, $name, $hair_color)
+  {
+    $this->type = $type;
+    $this->name = $name;
+    $this->hair_color = $hair_color;
+    //建構式內容
+  }
 
+  public function run()
+  {
+    echo "這隻名叫 {$this->name} 毛色是 {$this->hair_color} 的 {$this->type} 跑走了! ";
+    $this->speed();
+    //公開行為內容
+  }
 
+  private function speed()
+  {
+    echo "<br>";
+    echo "跑超級快!!";
+    //私有行為內容
+  }
+}
 
-
-
+$dog = new Animal('dog', 'John', "brown");
+$dog->run();
